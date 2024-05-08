1efefcfdeb5ec99a6065aa67dbc4ae20acfd25ec
Isbelis <iscaro07@gmail.com>
2024-05-08 16:13:27 -0500

    Update financial analysis

---
+++ PyBank/Main.py
@@ -0,0 +1,87 @@
+#**** Module 3-homework-Python challenge - PyBank*******
+
+#Dependencies
+import csv
+import os
+
+# Files to load and output 
+file_to_load = os.path.join("Resources", "budget_data.csv")
+file_to_output = os.path.join("Analysis", "Analysis_PyBank.txt")
+
+# Track various financial parameters
+total_months = 0
+month_of_change = []
+net_change_list = []
+greatest_increase = [("") ,0]
+greatest_decrease = [(""), 9999999999999999999999999999]
+total_net = 0
+
+
+#Read the csv and convert it into a list of dictionaies
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+with open(file_to_load) as financial_data:
+    reader = csv.reader(financial_data)
+    
+    #Read the header row
+    header =next(reader)
+    
+    # Extract first row to avoid appending to net_change_list
+    first_row = next(reader)
+    total_months+=1
+    total_net+= int(first_row[1])
+    prev_net = int(first_row[1])
+    
+    for row in reader:
+        
+        #Track the net change
+        total_months+=1
+        total_net+= int(row[1])
+        
+        # Trackt the net change 
+        net_change =int(row[1]) - prev_net
+        prev_net = int(row[1])
+        net_change_list += [net_change]
+        month_of_change +=[row[0]]
+        
+        # Calculate the greatest increase
+        if net_change > greatest_increase[1]:
+          greatest_increase[0] = row[0]
+          greatest_increase[1] = net_change
+          
+          # Calculate the greatest decrease
+        if net_change < greatest_decrease[1]:
+          greatest_decrease[0] = row[0]
+          greatest_decrease[1] = net_change
+          
+# Calculate the Average Net Change
+net_monthly_avg = round(sum(net_change_list) / len(net_change_list),2)
+
+
+#print (output, ("Financial Analysis\n")
+       #output += (f"Total months: {total_months}",
+                 # f"Total: ${total_net}"))
+
+
+print ("Financial Analysis", "")  
+print("Total months:", total_months)
+print("Total:", "$",total_net)
+print("Average Change:", "$",net_monthly_avg)
+print("Greatest Increase in Profits:", greatest_increase)
+print("Greatest Decrease in Profits:", greatest_decrease)
+          
+
+          
+        #   #Generate Output SUmmary
+        #   output = (
+        #       print (f:Analysis_PyBank.txt)
+        #   )
+
+          
+          
+          
+          
+            
+        
+        
+    
+    
