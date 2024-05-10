43c28576f4bfe0442826a75348bf5c4bdcf0882d
Isbelis <iscaro07@gmail.com>
2024-05-10 03:48:30 -0500

    Update

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -47,54 +47,49 @@ for name in List_candidates:
 # Set first candadidate on the dandidate_vote_list for loop
 candidates = List_candidates [0]
 
+for vote in List_candidates :
+        if candidates == vote:
+            total_voter += 1
+percent = total_voter/len(List_candidates)
+percent_candidate.append(percent)
+total_candidate.append(total_voter)
 
-print("Election Results")
-print("--------------------------")
-print(f"Total Votes: {count_of_votes}")
-print("--------------------------")
-with open(Election_analysis,"w") as txt_file:
- txt_file.write(output)
-print(output)
+if last_count < total_voter:
+        Winner = candidates
+print(f"{candidates}: {percent:.3%} ({total_voter})")
+
+    # Reset candidate votes to zero to name the winner
+last_count = total_voter
+total_voter = 0
 """
-#**************************************
-# Set variables
+output = ("----------\n"
+    "Election Results\n"
+    "-------------------------\n"
+    "--------------------------\n"
+    "Total Votes: {count_of_votes}\n"
+    "--------------------------\n"
+   for candidates in candidate: 
+       index = candidate.index(candidates)
+   f"{candidates}: {percent_candidate[index]:.3%} ({total_candidate[index]})\n"
+   "--------------------------\n"
+   f"Winner: {Winner}\n"
+   "---------------------\n")
+"""
+with open(Election_analysis,"w") as output_file:
+    output_file.write("Election Results\n")
+    output_file.write("-------------------------\n")
+    output_file.write(f"Total Votes: {count_of_votes}\n")
+    output_file.write("-------------------------\n")
+    for candidates in candidate: 
+        index = candidate.index(candidates)
+        output_file.write(f"{candidates}: {percent_candidate[index]:.3%} ({total_candidate[index]})\n ")
+    output_file.write("------------------------\n")
+    output_file.write(f"Winner: {Winner}\n")
+"""
+with open(Election_analysis,"w") as txt_file:
+    txt_file.write(output)
+print (output)
 
-
-
-# Set path for election_data csvfile
-Election_data= os.path.join("Resources", "election_data.csv")
-
-#Change current working directory
-os.chdir(os.path.dirname(os.path.realpath(__file__)))
-
-#Open and read election_data.csv
-with open(Election_data, encoding='UTF-8') as csvfile:
-    csv_reader = csv.reader(csvfile, delimiter=",")
-
-    # Read header row first
-    csv_header = next(csvfile)
- 
-    
-    for row in csv_reader:
-        List_candidates .append(row[2])
-
-    count_of_votes = len(List_candidates )
-#print(count_of_votes)
-
-# Name all candidates
-for name in List_candidates:
-    if name not in candidate:
-        candidate.append(name)
-        x = name
-
-# Set first candadidate on the dandidate_vote_list for loop
-candidates = List_candidates [0]
-
-# Print election results as demonstrated in homework
-print("Election Results")
-print("--------------------------")
-print(f"Total Votes: {count_of_votes}")
-print("--------------------------")
 
 # Set loop for list of candidates who recieved votes, percentage of votes, total num for each candidate, and winner
 for candidates in candidate:
